721a8b590844fec3d3bcc955c4461306ff90c372
Victor Coulon <victa.talawa@gmail.com>
2012-02-29 16:57:57 +0100

    Update curtain.js

--- curtain.js
+++ curtain.js
@@ -1,7 +1,7 @@
 /*
 * Curtain.js - Create an unique page transitioning system
 * ---
-* Version: 1.2
+* Version: 1.2.1
 * Copyright 2011, Victor Coulon (http://victorcoulon.fr)
 * Released under the MIT Licence
 */
